703ab975038cdbe866b038ed364bcc6582106c35
Jakub Kisielewski <kisielewskij66@gmail.com>
2026-07-31 00:27:23 +0100

    feat: implemented cancelOrder and init values for priceLevel

--- src/main/java/com/quant/clob/engine/PriceLevel.java
+++ src/main/java/com/quant/clob/engine/PriceLevel.java
@@ -1,9 +1,9 @@
 package com.quant.clob.engine;
 
 final class PriceLevel {
-    int priceLevel;
-    int size;
-    int totalVolume;
+    int priceLevel = 0;
+    int size = 0;
+    int totalVolume = 0;
     PriceLevel parent;
     PriceLevel leftChild;
     PriceLevel rightChild;
@@ -14,21 +14,33 @@ final class PriceLevel {
         order.parentPriceLevel = this;
         if (headOrder == null && tailOrder == null) {
             headOrder.nextOrder = tailOrder;
+            headOrder.prevOrder = null;
             headOrder = order;
             tailOrder.prevOrder = headOrder;
+            tailOrder.nextOrder = null;
             tailOrder = order;
+            size++;
+            totalVolume += order.shares;
+            priceLevel = order.limit;
         } else {
             order.prevOrder = tailOrder;
             tailOrder.nextOrder = order;
             tailOrder = order;
+            size++;
+            totalVolume += order.shares;
         }
     }
 
-    void cancelOrder(int idNumber) {
-
+    void cancelOrder(Order order) {
+        order.prevOrder.nextOrder = order.nextOrder;
+        order.nextOrder.prevOrder = order.prevOrder;
+        order.nextOrder = null;
+        order.prevOrder = null;
+        size--;
+        totalVolume -= order.shares;
     }
 
-    void executeOrder(int idNumber) {
+    void executeOrder(Order order) {
 
     }
 
