b0804d037a984562df80a60adecaa110713dee52
Jakub Kisielewski <kisielewskij66@gmail.com>
2026-07-31 00:26:18 +0100

    feat: naive adding pricelevel implementation

--- src/main/java/com/quant/clob/engine/OrderBook.java
+++ src/main/java/com/quant/clob/engine/OrderBook.java
@@ -5,4 +5,44 @@ final class OrderBook {
     PriceLevel sellTree;
     PriceLevel ask;
     PriceLevel bid;
+
+    void addOrderToBuyTree(Order order) {
+        PriceLevel priceLevel = priceLevelExists(buyTree, order.limit);
+        if (priceLevel == null) {
+            PriceLevel newPriceLevel = new PriceLevel();
+            newPriceLevel.addOrder(order);
+        } else {
+            priceLevel.addOrder(order);
+        }
+    }
+
+    PriceLevel priceLevelExists(PriceLevel currentPriceLevel, int price) {
+        if (currentPriceLevel == null) {
+            return null;
+        }
+
+        if (price == currentPriceLevel.priceLevel) {
+            return currentPriceLevel;
+        } else if (price > currentPriceLevel.priceLevel) {
+            return priceLevelExists(currentPriceLevel.rightChild, price);
+        } else {
+            return priceLevelExists(currentPriceLevel.leftChild, price);
+        }
+    }
+
+    void addPriceLevel(PriceLevel rootPriceLevel, PriceLevel newPriceLevel) {
+        if (newPriceLevel.priceLevel > rootPriceLevel.priceLevel) {
+            if (rootPriceLevel.rightChild == null) {
+                rootPriceLevel.rightChild = newPriceLevel;
+            } else {
+                addPriceLevel(rootPriceLevel.rightChild, newPriceLevel);
+            }
+        } else {
+            if (rootPriceLevel.leftChild == null) {
+                rootPriceLevel.leftChild = newPriceLevel;
+            } else {
+                addPriceLevel(rootPriceLevel.leftChild, newPriceLevel);
+            }
+        }
+    }
 }
