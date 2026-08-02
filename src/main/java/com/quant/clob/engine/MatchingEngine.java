da60ef08043a3e32c8e5622280452b227d982be5
Jakub Kisielewski <kisielewskij66@gmail.com>
2026-08-02 22:29:50 +0100

    chore: setup matching engine api

--- src/main/java/com/quant/clob/engine/MatchingEngine.java
+++ src/main/java/com/quant/clob/engine/MatchingEngine.java
@@ -1,1 +1,15 @@
 package com.quant.clob.engine;
+
+import java.util.Map;
+
+public final class MatchingEngine {
+
+  Map<Integer, Order> orders;
+  Map<Integer, PriceLevel> priceLevels;
+  OrderBook orderBook;
+
+  public void init() {
+
+  }
+  
+}
