88560dc0977bcf2b2172c5983f2a61b92fd5ba3f
Jakub Kisielewski <kisielewskij66@gmail.com>
2026-08-10 23:27:47 +0100

    feat: upadated adding order to include hashmap check

--- src/main/java/com/quant/clob/engine/MatchingEngine.java
+++ src/main/java/com/quant/clob/engine/MatchingEngine.java
@@ -5,7 +5,8 @@ import java.util.Map;
 public final class MatchingEngine {
 
   Map<Integer, Order> orders;
-  Map<Integer, PriceLevel> priceLevels;
+  Map<Integer, PriceLevel> buyPriceLevels;
+  Map<Integer, PriceLevel> sellPriceLevels;
   OrderBook orderBook;
 
   public void init() {
@@ -17,19 +18,39 @@ public final class MatchingEngine {
   }
   
   public void removeOrder(Order order) {
-    priceLevels.get(order.limit).size--;
-    priceLevels.get(order.limit).totalVolume -= order.shares;
+    if (order.isBuy) {
+      buyPriceLevels.get(order.limit).size--;
+      buyPriceLevels.get(order.limit).totalVolume -= order.shares;
+    } else {
+      sellPriceLevels.get(order.limit).size--;
+      sellPriceLevels.get(order.limit).totalVolume -= order.shares;
+    }
+
     Order.freeOrderObject(order);
   }
   
   public void addOrder(Order order) {
     PriceLevel priceLevel;
     if (order.isBuy) {
-      priceLevel = orderBook.addOrderToBuyTree(order);
+      priceLevel = buyPriceLevels.get(order.limit);
+
+      if (priceLevel != null) {
+        priceLevel.addOrder(order);
+      } else {
+        priceLevel = orderBook.addOrderToBuyTree(order);
+        buyPriceLevels.put(priceLevel.priceLevel, priceLevel);
+      }
+
     } else {
-      priceLevel = orderBook.addOrderToBuyTree(order);
+      priceLevel = sellPriceLevels.get(order.limit);
+
+      if (priceLevel != null) {
+        priceLevel.addOrder(order);
+      } else {
+        priceLevel = orderBook.addOrderToSellTree(order);
+        sellPriceLevels.put(priceLevel.priceLevel, priceLevel);
+      }
     }
-    priceLevels.put(priceLevel.priceLevel, priceLevel);
     orders.put(order.idNumber, order);
   }
 }
