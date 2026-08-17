9d4b595dd487f96a369d04b063e05aebb89afd2f
Jakub Kisielewski <kisielewskij66@gmail.com>
2026-08-18 00:19:45 +0100

    feat: implemented adding/removing order at pricelevel

--- src/main/java/com/quant/clob/engine/PriceLevel.java
+++ src/main/java/com/quant/clob/engine/PriceLevel.java
@@ -3,52 +3,36 @@ package com.quant.clob.engine;
 import java.util.ArrayList;
 import java.util.List;
 
-public final class PriceLevel {
-    public int priceLevel = 0;
+final class PriceLevel {
+    int priceLevel = 0;
     int size = 0;
     int totalVolume = 0;
     PriceLevel parent;
     PriceLevel leftChild;
     PriceLevel rightChild;
     Order headOrder; // oldest order
-    public Order tailOrder; // newest order
+    Order tailOrder; // newest order
 
-    public PriceLevel() {
-        this.priceLevel = 0;
-        this.size = 0;
-        this.totalVolume = 0;
-        this.parent = null;
-        this.leftChild = null;
-        this.rightChild = null;
-        this.headOrder = new Order(this);
-
-        Order newTail = new Order(this);
-        newTail.prevOrder = this.headOrder;
-        newTail.idNumber = 1;
-        this.tailOrder = newTail;
-
-        this.headOrder.nextOrder = this.tailOrder;
+    PriceLevel() {
     }
 
     void addOrder(Order order) {
         order.parentPriceLevel = this;
-        if (headOrder == null && tailOrder == null) {
-            headOrder.nextOrder = tailOrder;
-            headOrder.prevOrder = null;
-            headOrder = order;
-            tailOrder.prevOrder = headOrder;
-            tailOrder.nextOrder = null;
-            tailOrder = order;
-            size++;
-            totalVolume += order.shares;
-            priceLevel = order.limit;
+        if (this.isEmpty()) {
+            this.headOrder = order;
+            this.headOrder.nextOrder = tailOrder;
         } else {
-            order.prevOrder = tailOrder;
-            tailOrder.nextOrder = order;
-            tailOrder = order;
-            size++;
-            totalVolume += order.shares;
+            if (this.size == 1) {
+                this.tailOrder = order;
+                this.tailOrder.prevOrder = headOrder;
+            } else {
+                this.tailOrder.nextOrder = order;
+                order.prevOrder = this.tailOrder;
+                this.tailOrder = order;
+            }
         }
+        size++;
+        totalVolume += order.shares;
     }
 
     void removeOrder(Order order) {
@@ -56,8 +40,11 @@ public final class PriceLevel {
             this.totalVolume -= headOrder.shares;
             headOrder = headOrder.nextOrder;
             Order.freeOrderObject(headOrder.prevOrder);
+            headOrder.prevOrder = null;
         } else {
             this.totalVolume -= order.shares;
+            order.prevOrder.nextOrder = order.nextOrder;
+            order.nextOrder.prevOrder = order.prevOrder;
             Order.freeOrderObject(order);
         }
         this.size--;
@@ -103,7 +90,7 @@ public final class PriceLevel {
         return output.toString();
     }
     
-    public List<Integer> orderIds(List<Integer> output, Order currentOrder) {
+    List<Integer> orderIds(List<Integer> output, Order currentOrder) {
         if (currentOrder.nextOrder == null) {
             output.add(currentOrder.idNumber);
             return output;
