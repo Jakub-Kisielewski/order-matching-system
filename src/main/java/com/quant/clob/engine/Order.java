ac5ccae84bbea9a1ee7d7c4538f77bf9f9ee56b0
Jakub Kisielewski <kisielewskij66@gmail.com>
2026-08-18 00:21:12 +0100

    chore: code cleanup

--- src/main/java/com/quant/clob/engine/Order.java
+++ src/main/java/com/quant/clob/engine/Order.java
@@ -1,7 +1,7 @@
 package com.quant.clob.engine;
 
-public final class Order {
-	public int idNumber;
+final class Order {
+	int idNumber;
 	boolean isBuy;
 	boolean isMarketOrder; // market order fills volume completely, limit order fills up to limit and is then added to tree
 	int shares;
@@ -12,17 +12,7 @@ public final class Order {
 	Order prevOrder;
 	PriceLevel parentPriceLevel;
 
-	public Order(PriceLevel parentPriceLevel) {
-		this.idNumber = 0;
-		this.isBuy = true;
-		this.isMarketOrder = false;
-		this.shares = 10;
-		this.limit = 20;
-		this.entryTime = 0;
-		this.eventTime = 0;
-		this.nextOrder = null;
-		this.prevOrder = null;
-		this.parentPriceLevel = parentPriceLevel;
+	Order(PriceLevel parentPriceLevel) {
 	}
 
 	static void freeOrderObject(Order order) {
