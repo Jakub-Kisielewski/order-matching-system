3b3aec0b485bf89b8453ef217706370c3bec8989
Jakub Kisielewski <kisielewskij66@gmail.com>
2026-09-06 17:15:44 +0100

    test: added unit tests for all methods positive paths priceLevel

--- src/test/java/com/quant/clob/engine/PriceLevelTest.java
+++ src/test/java/com/quant/clob/engine/PriceLevelTest.java
@@ -23,7 +23,7 @@ class PriceLevelTest {
     priceLevel.addOrder(order);
     priceLevel.removeOrder(order);
     assertEquals(0, priceLevel.size);
-    assertEquals(order, priceLevel.headOrder);
+    assertEquals(null, priceLevel.headOrder);
     assertEquals(0, priceLevel.totalVolume);
   }
 
@@ -38,5 +38,76 @@ class PriceLevelTest {
     assertEquals(1, priceLevel.size);
     assertEquals(order2, priceLevel.headOrder);
     assertEquals(order2.shares, priceLevel.totalVolume);
+  }
+
+  @Test
+  void removeOrder_RemoveTailOrder_Success() {
+    PriceLevel priceLevel = new PriceLevel();
+    Order order1 = new Order();
+    Order order2 = new Order();
+    priceLevel.addOrder(order1);
+    priceLevel.addOrder(order2);
+    priceLevel.removeOrder(order2);
+    assertEquals(1, priceLevel.size);
+    assertEquals(order1, priceLevel.headOrder);
+    assertEquals(order1.shares, priceLevel.totalVolume);
+  }
+
+  @Test
+  void removeOrder_RemoveMiddleOrder_Success() {
+    PriceLevel priceLevel = new PriceLevel();
+    Order order1 = new Order();
+    Order order2 = new Order();
+    Order order3 = new Order();
+    priceLevel.addOrder(order1);
+    priceLevel.addOrder(order2);
+    priceLevel.addOrder(order3);
+    priceLevel.removeOrder(order2);
+    assertEquals(2, priceLevel.size);
+    assertEquals(order1, priceLevel.headOrder);
+    assertEquals(order3, priceLevel.tailOrder);
+    assertEquals(order1.shares + order3.shares, priceLevel.totalVolume);
+  }
+
+  @Test 
+  void fillOrder_Success() {
+    PriceLevel priceLevel = new PriceLevel();
+    Order order1 = new Order();
+    order1.shares = 10;
+    Order orderToFill = new Order();
+    orderToFill.shares = 10;
+    priceLevel.addOrder(order1);
+    priceLevel.fillOrder(orderToFill);
+    assertEquals(0, priceLevel.size);
+    assertEquals(0, orderToFill.shares);
+  }
+
+  @Test 
+  void fillOrder_NotFilled_Success() {
+    PriceLevel priceLevel = new PriceLevel();
+    Order order1 = new Order();
+    order1.shares = 5;
+    Order orderToFill = new Order();
+    orderToFill.shares = 10;
+    priceLevel.addOrder(order1);
+    priceLevel.fillOrder(orderToFill);
+    assertEquals(0, priceLevel.size);
+    assertEquals(5, orderToFill.shares);
+  }
+
+  @Test
+  void fillOrder_FilledMultiOrder_Success() {
+    PriceLevel priceLevel = new PriceLevel();
+    Order order1 = new Order();
+    order1.shares = 5;
+    Order order2 = new Order();
+    order2.shares = 5;
+    Order orderToFill = new Order();
+    orderToFill.shares = 10;
+    priceLevel.addOrder(order1);
+    priceLevel.addOrder(order2);
+    priceLevel.fillOrder(orderToFill);
+    assertEquals(0, priceLevel.size);
+    assertEquals(0, orderToFill.shares);
   }
 }
